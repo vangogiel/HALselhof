736d4e502e6aeea4550069b447eb5c1b39b4af2c
Norbert Gogiel <norbert.gogiel@fisglobal.com>
2021-11-24 22:59:34 +0000

    Update unit tests to provide coverage

--- src/test/scala/play/api/hal/HalBuilderSpec.scala
+++ src/test/scala/play/api/hal/HalBuilderSpec.scala
@@ -238,5 +238,63 @@ class HalBuilderSpec extends PlaySpec {
           "status": "shipped"
         }""".stripMargin)
     }
+
+    "build json with multiple links in a relation and embedded data" in {
+      Hal()
+        .withRelation("self", HalHref("/orders"))
+        .withRelation("next", HalHref("/orders?page=2"))
+        .withRelation("find", HalHref("/orders{?id}").withTemplated())
+        .withRelation(
+          "curies",
+          Seq(
+            HalHref("/order/1").withName("1").withTemplated(),
+            HalHref("/order/2").withName("2").withTemplated(),
+            HalHref("/order/3").withName("3").withTemplated()
+          )
+        )
+        .withEmbedded(
+          "orders",
+          TestData(30, "USD", "shipped").asResource ++
+            HalRelation("self", "/orders/123") ++
+            HalRelation("basket", "/baskets/98712") ++
+            HalRelation("customer", "/customers/7809"),
+          TestData(20, "USD", "processing").asResource ++
+            HalRelation("self", "/orders/124") ++
+            HalRelation("basket", "/baskets/97213") ++
+            HalRelation("customer", "/customers/12369")
+        )
+        .buildJson() mustBe Json.parse("""{
+          "_links": {
+              "self": { "href": "/orders" },
+              "next": { "href": "/orders?page=2" },
+              "find": { "href": "/orders{?id}", "templated": true },
+              "curies" : [
+                  { "href": "/order/1", "name": "1", "templated": true},
+                  { "href": "/order/2", "name": "2", "templated": true},
+                  { "href": "/order/3", "name": "3", "templated": true}
+          ]},
+          "_embedded": {
+            "orders": [{
+                "_links": {
+                  "self": { "href": "/orders/123" },
+                  "basket": { "href": "/baskets/98712" },
+                  "customer": { "href": "/customers/7809" }
+                },
+                "total": 30,
+                "currency": "USD",
+                "status": "shipped"
+              },{
+                "_links": {
+                  "self": { "href": "/orders/124" },
+                  "basket": { "href": "/baskets/97213" },
+                  "customer": { "href": "/customers/12369" }
+                },
+                "total": 20,
+                "currency": "USD",
+                "status": "processing"
+            }]
+          }
+        }""".stripMargin)
+    }
   }
 }
